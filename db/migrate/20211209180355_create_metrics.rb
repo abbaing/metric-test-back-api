52a7bfa6c22ded511ca16bbc312ce359d2298d16
Ing. Abba David <david_abba@outlook.com>
2021-12-09 15:20:28 -0300

    Updated metric migration

--- db/migrate/20211209180355_create_metrics.rb
+++ db/migrate/20211209180355_create_metrics.rb
@@ -1,8 +1,9 @@
 class CreateMetrics < ActiveRecord::Migration[6.1]
   def change
     create_table :metrics do |t|
-      
-      t.timestamps
+      t.string :name
+      t.decimal :value
+      t.timestamp :time
     end
   end
 end
