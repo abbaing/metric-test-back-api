8e2517f4c08b9e7dfedb3b11f536d6246eb4618a
Ing. Abba David <david_abba@outlook.com>
2021-12-09 15:24:19 -0300

    Updated metric controller

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,2 +1,37 @@
 class MetricsController < ApplicationController
+    def index
+        @metrics = Metric.all 
+        render json: @metrics
+    end 
+
+    def show
+        @metric = Metric.find(params[:id])
+        render json: @metric
+    end 
+
+    def create
+        @metric = Metric.create(
+            name: params[:name],
+            value: params[:value],
+            time: params[:time]
+        )
+        render json: @metric
+    end 
+
+    def update
+        @metric = Metric.find(params[:id])
+        @metric.update(
+            name: params[:name],
+            value: params[:value],
+            time: params[:time]
+        )
+        render json: @metric
+    end 
+
+    def destroy
+        @metrics = Metric.all 
+        @metric = Metric.find(params[:id])
+        @metric.destroy
+        render json: @metrics
+    end 
 end
