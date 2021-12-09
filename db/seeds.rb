206468229f08a7e669f9a5011c4954cb377dc950
Ing. Abba David <david_abba@outlook.com>
2021-12-09 15:29:13 -0300

    Added seed data to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+metric1 = Metric.create(name: "Metric 1", value: 150.0, time: DateTime.strptime("12/09/2021 15:00", "%m/%d/%Y %H:%M"))
+metric1 = Metric.create(name: "Metric 2", value: 45.5, time: DateTime.strptime("12/09/2021 18:00", "%m/%d/%Y %H:%M"))
+metric1 = Metric.create(name: "Metric 3", value: 17.72, time: DateTime.strptime("12/09/2021 21:00", "%m/%d/%Y %H:%M"))
