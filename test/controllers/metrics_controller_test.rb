05f8c03eef8a11e2884dc824a600ae590a4a2bfb
Ing. Abba David <david_abba@outlook.com>
2021-12-16 02:00:40 -0300

    Integrated fixture to tests

--- test/controllers/metrics_controller_test.rb
+++ test/controllers/metrics_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class MetricsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :metrics
+
+  test "Metric controller get all" do
+    get "/metrics"
+    assert_response :success
+    assert_equal 200, status
+  end
+
+  test "Metric controller add" do  
+    post "/metrics",
+      params: { metric: :one }
+    assert_response :success
+    assert_equal 200, status
+  end
+  
 end
