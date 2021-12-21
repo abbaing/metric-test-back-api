d5c93c73ec74b9907d96a77c8924ebd3310aa6a2
Ing. Abba David <david_abba@outlook.com>
2021-12-21 02:14:16 -0300

    Handling error was added

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,37 +1,57 @@
 class MetricsController < ApplicationController
     def index
-        @metrics = Metric.all.order(time: :desc)
-        render json: @metrics
+        begin
+            @metrics = Metric.all.order(time: :desc)
+            render json: @metrics
+        rescue => e
+            print e
+        end
     end 
 
     def show
-        @metric = Metric.find(params[:id])
-        render json: @metric
+        begin
+            @metric = Metric.find(params[:id])
+            render json: @metric
+        rescue => e
+            print e
+        end
     end 
 
     def create
-        @metric = Metric.create(
-            name: params[:name],
-            value: params[:value],
-            time: params[:time]
-        )
-        render json: @metric
+        begin
+            @metric = Metric.create(
+                name: params[:name],
+                value: params[:value],
+                time: params[:time]
+            )
+            render json: @metric
+        rescue => e
+            print e
+        end
     end 
 
     def update
-        @metric = Metric.find(params[:id])
-        @metric.update(
-            name: params[:name],
-            value: params[:value],
-            time: params[:time]
-        )
-        render json: @metric
+        begin
+            @metric = Metric.find(params[:id])
+            @metric.update(
+                name: params[:name],
+                value: params[:value],
+                time: params[:time]
+            )
+            render json: @metric
+        rescue => e
+            print e
+        end
     end 
 
     def destroy
-        @metrics = Metric.all 
-        @metric = Metric.find(params[:id])
-        @metric.destroy
-        render json: @metrics
+        begin
+            @metrics = Metric.all 
+            @metric = Metric.find(params[:id])
+            @metric.destroy
+            render json: @metrics
+        rescue => e
+            print e
+        end
     end 
 end
