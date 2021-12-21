e4ba65ccdabeefee78900355a98fdb89f8953aee
Ing. Abba David <david_abba@outlook.com>
2021-12-20 23:43:30 -0300

    Added test for calculation controller

--- test/controllers/calculation_controller_test.rb
+++ test/controllers/calculation_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class CalculationControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :metrics
+
+  test "Calculation average per minute specific result" do
+    get "/calculation?averageperminute=true"
+    assert_equal "\"20.0\"", @response.body
+  end
+
+  test "Calculation average per hour specific result" do
+    get "/calculation?averageperhour=true"
+    assert_equal "\"15.0\"", @response.body
+  end
+
+  test "Calculation average per day specific result" do  
+    get "/calculation?averageperday=true"
+    assert_equal "\"11.66666666666667\"", @response.body
+  end
+
+  test "Calculation average per minute" do
+    get "/calculation?averageperminute=true"
+    assert_response :success
+    assert_equal 200, status
+  end
+
+  test "Calculation average per hour" do
+    get "/calculation?averageperhour=true"
+    assert_response :success
+    assert_equal 200, status
+  end
+
+  test "Calculation average per day" do  
+    get "/calculation?averageperday=true"
+    assert_response :success
+    assert_equal 200, status
+  end
 end
